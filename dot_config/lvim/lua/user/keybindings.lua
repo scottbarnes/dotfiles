8137ef41cda85648bf1f9a4a58a5a230fd9e44de
Scott Barnes <scottreidbarnes@gmail.com>
2023-11-19 08:53:09 -0800

    bring chezmoi into line with existing configs

--- dot_config/lvim/lua/user/keybindings.lua
+++ dot_config/lvim/lua/user/keybindings.lua
@@ -14,7 +14,15 @@ M.config = function()
   vim.cmd("nnoremap Q <nop>")
   vim.cmd("nnoremap <silent> <c-l> :nohlsearch<cr><c-l>")
 
-
+  lvim.builtin.which_key.mappings["t"] = {
+    name = "Diagnostics",
+    t = { "<cmd>TroubleToggle<cr>", "trouble" },
+    w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+    d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+    q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+    l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+    r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+  }
 end
 
 return M
--- dot_config/lvim/lua/user/plugins.lua
+++ dot_config/lvim/lua/user/plugins.lua
@@ -3,7 +3,6 @@
 local M = {}
 
 M.config = function()
-
   lvim.plugins = {
     { "folke/tokyonight.nvim" },
     { "sainnhe/everforest" },
@@ -44,33 +43,101 @@ M.config = function()
         require('dap.ext.vscode').load_launchjs()
       end,
     },
-    { "leoluz/nvim-dap-go", module = "dap-go" },
-    { "ray-x/go.nvim",
+    { "leoluz/nvim-dap-go", },
+    {
+      "ray-x/go.nvim",
       config = function()
         require('go').setup({
-            goimport = 'gopls', -- if set to 'gopls' will use golsp format
-            gofmt = 'gopls', -- if set to gopls will use golsp format
-            max_line_len = 120,
-            tag_transform = false,
-            test_dir = '',
-            comment_placeholder = '   ',
-            lsp_cfg = true, -- false: use your own lspconfig
-            lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
-            lsp_on_attach = true, -- use on_attach from go.nvim
-            dap_debug = true,
+          goimport = 'gopls', -- if set to 'gopls' will use golsp format
+          gofmt = 'gopls',    -- if set to gopls will use golsp format
+          max_line_len = 120,
+          tag_transform = false,
+          test_dir = '',
+          comment_placeholder = '   ',
+          lsp_cfg = true,       -- false: use your own lspconfig
+          lsp_gofumpt = true,   -- true: set default gofmt in gopls format to gofumpt
+          lsp_on_attach = true, -- use on_attach from go.nvim
+          dap_debug = true,
         })
         -- Run gofmt + goimport on save
         vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
       end,
     },
-    { 'ray-x/guihua.lua',
-    config = function()
-      require('guihua.maps').setup({
-        maps = {
-          close_view = '<C-x>',
+    {
+      'ray-x/guihua.lua',
+      config = function()
+        require('guihua.maps').setup({
+          maps = {
+            close_view = '<C-x>',
+          }
+        })
+      end,
+    },
+    {
+      "kevinhwang91/nvim-bqf",
+      event = { "BufRead", "BufNew" },
+      config = function()
+        require("bqf").setup({
+          auto_enable = true,
+          preview = {
+            win_height = 12,
+            win_vheight = 12,
+            delay_syntax = 80,
+            border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+          },
+          func_map = {
+            vsplit = "",
+            ptogglemode = "z,",
+            stoggleup = "",
+          },
+          filter = {
+            fzf = {
+              action_for = { ["ctrl-s"] = "split" },
+              extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+            },
+          },
+        })
+      end,
+    },
+    {
+      "folke/trouble.nvim",
+      cmd = "TroubleToggle",
+    },
+    {
+      "folke/todo-comments.nvim",
+      event = "BufRead",
+      config = function()
+        require("todo-comments").setup()
+      end,
+    },
+    {
+      'scalameta/nvim-metals',
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        local lvim_lsp = require("lvim.lsp")
+        local metals_config = require("metals").bare_config()
+        metals_config.on_init = lvim_lsp.common_on_init
+        metals_config.on_exit = lvim_lsp.common_on_exit
+        metals_config.capabilities = lvim_lsp.common_capabilities()
+        metals_config.on_attach = function(client, bufnr)
+          lvim_lsp.common_on_attach(client, bufnr)
+          require("metals").setup_dap()
+        end
+        metals_config.settings = {
+          superMethodLensesEnabled = true,
+          showImplicitArguments = true,
+          showInferredType = true,
+          showImplicitConversionsAndClasses = true,
+          excludedPackages = {},
         }
-      })
-      end,},
+        metals_config.init_options.statusBarProvider = false
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = { "scala", "sbt", "java" },
+          callback = function() require("metals").initialize_or_attach(metals_config) end,
+          group = vim.api.nvim_create_augroup("nvim-metals", { clear = true }),
+        })
+      end
+    },
     -- {
     --   "rcarriga/nvim-dap-ui",
     --   config = function()
@@ -82,7 +149,6 @@ M.config = function()
     --   disable = not lvim.builtin.dap.active,
     -- },
   }
-
 end
 
 return M
