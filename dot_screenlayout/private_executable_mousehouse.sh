b0528fea51b627e0b2a4940a6ce455ced12875af
Scott Barnes <scottreidbarnes@gmail.com>
2023-12-17 23:05:53 -0800

    Add ~/.screenlayout for mousehouse

---
+++ dot_screenlayout/private_executable_mousehouse.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-1 --off --output DP-1 --mode 2560x1440 --pos 3640x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 2560x0 --rotate left --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-2 --off --output DP-1-3 --off --output HDMI-1-3 --off
